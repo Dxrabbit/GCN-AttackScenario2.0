d6e85af00a58e659346a3f91f8509b00049ed95f
Dxrabbit <78236273@qq.com>
2021-07-23 11:38:11 +0800

    init the repo

---
+++ pyGCN/train.py
@@ -0,0 +1,165 @@
+from __future__ import division
+from __future__ import print_function
+
+import time
+import argparse
+import numpy as np
+
+import torch
+import torch.nn.functional as F
+import torch.optim as optim
+
+from pyGCN.utils import load_data, accuracy
+from pyGCN.models import GCN
+
+import matplotlib.pyplot as plt
+from mpl_toolkits.axes_grid1 import host_subplot
+
+'''
+定义一个显示超参数的函数，将代码中所有的超参数打印
+'''
+def show_Hyperparameter(args):
+    argsDict = args.__dict__
+    print(argsDict)
+    print('the settings are as following')
+    for key in argsDict:
+        print(key,':',argsDict[key])
+
+
+# Training settings
+parser = argparse.ArgumentParser()
+parser.add_argument('--no-cuda', action='store_true', default=False,
+                    help='Disables CUDA training.')
+parser.add_argument('--fastmode', action='store_true', default=False,
+                    help='Validate during training pass.')
+parser.add_argument('--seed', type=int, default=42, help='Random seed.')
+parser.add_argument('--epochs', type=int, default=500,
+                    help='Number of epochs to train.')
+parser.add_argument('--lr', type=float, default=0.001,
+                    help='Initial learning rate.')
+parser.add_argument('--weight_decay', type=float, default=5e-4,
+                    help='Weight decay (L2 loss on parameters).')
+parser.add_argument('--hidden', type=int, default=16,
+                    help='Number of hidden units.')
+parser.add_argument('--dropout', type=float, default=0.5,
+                    help='Dropout rate (1 - keep probability).')
+
+args = parser.parse_args()
+args.cuda = not args.no_cuda and torch.cuda.is_available()
+
+np.random.seed(args.seed)
+torch.manual_seed(args.seed)
+if args.cuda:
+    torch.cuda.manual_seed(args.seed)
+
+# Load data
+adj, features, labels, idx_train, idx_val, idx_test = load_data()
+
+# Model and optimizer
+model = GCN(nfeat=features.shape[1],
+            nhid=args.hidden,
+            nclass=labels.max().item() + 1,
+            dropout=args.dropout)
+optimizer = optim.Adam(model.parameters(),
+                       lr=args.lr, weight_decay=args.weight_decay)
+
+if args.cuda:
+    model.cuda()
+    features = features.cuda()
+    adj = adj.cuda()
+    labels = labels.cuda()
+    idx_train = idx_train.cuda()
+    idx_val = idx_val.cuda()
+    idx_test = idx_test.cuda()
+
+train_loss_list = []
+train_acc_list = []
+eval_loss_list = []
+eval_acc_list = []
+test_loss_list = []
+test_acc_list = []
+
+def train(epoch):
+    t = time.time()
+    model.train()
+    optimizer.zero_grad()
+    output = model(features, adj)
+    loss_train = F.nll_loss(output[idx_train], labels[idx_train])#交叉熵
+    acc_train = accuracy(output[idx_train], labels[idx_train])
+    loss_train.backward()
+    optimizer.step()
+    train_loss_list.append(loss_train)
+    train_acc_list.append(acc_train)
+
+    if not args.fastmode:
+        # Evaluate validation set performance separately,
+        # deactivates dropout during validation run.
+        model.eval()
+        output = model(features, adj)
+
+    loss_val = F.nll_loss(output[idx_val], labels[idx_val])
+    acc_val = accuracy(output[idx_val], labels[idx_val])
+
+    # save testint result...
+    eval_loss_list.append(loss_val)
+    eval_acc_list.append(acc_val)
+
+    print('Epoch: {:04d}'.format(epoch+1),
+          'loss_train: {:.4f}'.format(loss_train.item()),
+          'acc_train: {:.4f}'.format(acc_train.item()),
+          'loss_val: {:.4f}'.format(loss_val.item()),
+          'acc_val: {:.4f}'.format(acc_val.item()),
+          'time: {:.4f}s'.format(time.time() - t))
+
+
+def test():
+    model.eval()
+    output = model(features, adj)
+    loss_test = F.nll_loss(output[idx_test], labels[idx_test])
+    acc_test = accuracy(output[idx_test], labels[idx_test])
+    print("Test set results:",
+          "loss= {:.4f}".format(loss_test.item()),
+          "accuracy= {:.4f}".format(acc_test.item()))
+
+#绘制曲线
+def plot_acc_loss(loss, acc,name):
+    host = host_subplot(111)  # row=1 col=1 first pic
+    plt.subplots_adjust(right=0.8)  # ajust the right boundary of the plot window
+    par1 = host.twinx()   # 共享x轴
+ 
+    # set labels
+    host.set_xlabel("steps")
+    host.set_ylabel(name+"-loss")
+    par1.set_ylabel(name+"-accuracy")
+ 
+    # plot curves
+    p1, = host.plot(range(len(loss)), loss, label="loss")
+    p2, = par1.plot(range(len(acc)), acc, label="accuracy")
+ 
+    # set location of the legend,
+    # 1->rightup corner, 2->leftup corner, 3->leftdown corner
+    # 4->rightdown corner, 5->rightmid ...
+    host.legend(loc=5)
+ 
+    # set label color
+    host.axis["left"].label.set_color(p1.get_color())
+    par1.axis["right"].label.set_color(p2.get_color())
+ 
+    # set the range of x axis of host and y axis of par1
+    # host.set_xlim([-200, 5200])
+    # par1.set_ylim([-0.1, 1.1])
+ 
+    plt.draw()
+    plt.show()
+
+# Train model
+t_total = time.time()
+for epoch in range(args.epochs):
+    train(epoch)
+print("Optimization Finished!")
+print("Total time elapsed: {:.4f}s".format(time.time() - t_total))
+
+# Testing
+test()
+plot_acc_loss(eval_loss_list, eval_acc_list,"val")
+plot_acc_loss(train_loss_list, train_acc_list,"train")
